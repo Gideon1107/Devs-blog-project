dc0eb72e1edb17e6b974ba8a2648be61a92f3744
Robert Minkler <83042739+rminkler1@users.noreply.github.com>
2024-03-29 20:10:46 -0700

    Contact form email bug fix
    Flipped the send to / receive email addresses.

--- main.py
+++ main.py
@@ -266,8 +266,8 @@ def contact():
         with SMTP("smtp.gmail.com", 465) as connection:
             connection.login(MY_EMAIL, PASSWORD)
             connection.sendmail(
-                from_addr=MY_EMAIL,
-                to_addrs=email,
+                from_addr=email,
+                to_addrs=MY_EMAIL,
                 msg=f"Subject: New Message from blog \n\nName: {name}\nEmail: {email}\nPhone Number: {phone}\nMessage: {message}"
             )
         return render_template("contact.html", year=current_date, msg_sent=True, logged_in=current_user.is_authenticated)
